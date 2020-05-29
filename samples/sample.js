0fd99dc616d1e1739de6c119527ca0c6f9c89daf
Maik Riechert <maik.riechert@arcor.de>
2020-05-29 20:35:29 +0100

    simplify sample and handle ctrl-c

--- samples/sample.js
+++ samples/sample.js
@@ -4,35 +4,25 @@ const width = 1280;
 const height = 720;
 const fps = 30;
 const delay = 10;
+const frameMs = 1/fps * 1000;
 
-function sleep(secs) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, secs / 1000);
-    });
-}
+let timerId;
 
-async function main() {
-    virtualcam.start(width, height, fps, delay);
-    console.log(`virtual cam output started (${width}x${height} @ ${fps}fps)`);
-    try {
-        let i = 0;
-        while (true) {
+// Catch ctrl-c.
+process.on('SIGINT', () => {
+    clearInterval(timerId);
+    virtualcam.stop();
+    console.log('virtual cam output stopped');
+    process.exit(1);
+});
 
-            const frame = new Uint8Array(height * width * 4);
-            frame.fill(i % 255);
+virtualcam.start(width, height, fps, delay);
+console.log(`virtual cam output started (${width}x${height} @ ${fps}fps)`);
 
-            // Send to the virtual cam.
-            virtualcam.send(i, frame);
-
-            // Wait until it's time to prepare the next frame.
-            sleep(1/fps);
-
-            i += 1;
-        }
-    } finally {
-        virtualcam.stop();
-        console.log('virtual cam output stopped');
-    }
-}
-
-main();
+let i = 0;
+timerId = setInterval(() => {
+    const frame = new Uint8Array(height * width * 4);
+    frame.fill(i % 255);
+    virtualcam.send(i, frame);
+    i += 1;
+}, frameMs);
